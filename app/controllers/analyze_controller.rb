45ea301071099da1449bc4973e87c14ca96f6dbe
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-03-16 16:25:06 +0900

    フロントエンドからバックエンドに診断条件の値を全て渡す

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -1,5 +1,5 @@
 class AnalyzeController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create]
+    # skip_before_action :verify_authenticity_token, only: [:create]
   
     def create
 
@@ -7,16 +7,17 @@ class AnalyzeController < ApplicationController
         puts "Received parameters: #{params.inspect}"
       
         # フロントエンドから送信されたデータを受け取る
-      budget = params[:budget]
-      has_caffeine = params[:hasCaffeine]
-      feeling = params[:feeling]
-      commitment = params[:commitment]
-      drink_size = params[:drinkSize]
+        budget = params[:budget]
+        has_not_caffeine = params[:hasNotCaffeine]
+        feeling = params[:feeling]
+        commitment = params[:commitment]
+        drink_size = params[:drinkSize]
   
-      # 受け取ったデータをもとに何らかの分析を行う（ここではダミーのレスポンスを返す）
-      analysis_result = "Analyzed: #{budget}, #{has_caffeine}, #{feeling}, #{commitment}, #{drink_size}"
-  
-      # 分析結果をJSONとしてフロントエンドに返す
-      render json: { result: analysis_result }
+        # 受け取ったデータをもとに何らかの分析を行う（ここではダミーのレスポンスを返す）
+        analysis_result = "Analysis conditions : #{budget}, #{has_not_caffeine}, #{feeling}, #{commitment}, #{drink_size}"
+    
+        # 分析結果をJSONとしてフロントエンドに返す
+        render json: { result: analysis_result }
+
     end
   end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:8000"
+    # origins "*"
 
     resource "*",
       headers: :any,
