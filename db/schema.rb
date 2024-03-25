e65b2203c31fe879a819a43a13e9483f7b14573b
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-03-25 20:49:37 +0900

    “カスタムテーブルを作ってしまったのでひとまずコミット”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_26_210359) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_25_112519) do
+  create_table "customs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.float "calorie"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
