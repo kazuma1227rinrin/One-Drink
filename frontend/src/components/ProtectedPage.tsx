cefb3ef3cac6b50835d3345195acfd0451b69846
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-05-12 16:05:15 +0900

    “未ログイン状態でのページアクセス時に、ログイン画面へ遷移する処理を追加”

--- frontend/src/components/ProtectedPage.tsx
+++ frontend/src/components/ProtectedPage.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from '@/contexts/AuthProvider';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 interface ProtectedPageProps {
     children: ReactNode;
@@ -7,11 +8,33 @@ interface ProtectedPageProps {
 
 const ProtectedPage: React.FC<ProtectedPageProps> = ({children}) => {
   const { user } = useAuth();
+  const router = useRouter();
 
   console.log('Current user:', user); // ユーザー情報をログに出力
 
+  useEffect(() => {
+    if (!user) {
+      const timer = setTimeout(() => {
+        router.push('/'); // ログインページへ遷移
+      }, 3000); // 3秒後に実行
+
+      return () => clearTimeout(timer); // コンポーネントのアンマウント時にタイマーをクリア
+    }
+  }, [user, router]);
+
   if (!user) {
-    return <div>Loading or not authenticated...</div>;
+    return (
+      <div style={{
+        height: "100vh",  // ビューポートの高さに合わせる
+        display: "flex",
+        justifyContent: "center",  // 水平方向の中央揃え
+        alignItems: "center",  // 垂直方向の中央揃え
+        fontSize: "15px",  // フォントサイズ
+        textAlign: "center"  // テキストを中央に揃える
+      }}>
+        ログインしてください。3秒後にログイン画面に移動します。
+      </div>
+    );
   }
 
   return <>{children}</>;
