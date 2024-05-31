0fd5e0b2e6ca73ef5fc7e43820a6df0462c6db42
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-05-31 17:29:47 +0900

    Deploying backend to Heroku with JawsDB MySQL

--- config/puma.rb
+++ config/puma.rb
@@ -44,7 +44,7 @@ plugin :tmp_restart
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-rackup DefaultRackup
+# rackup DefaultRackup
 port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
