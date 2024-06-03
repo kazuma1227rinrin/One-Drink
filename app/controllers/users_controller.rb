0253ac1d2580f798b210e0c47dbfb652f7bc4391
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-06-03 13:12:51 +0900

    usersコントローラにCORSのヘッダを手動で記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,36 @@
 class UsersController < ApplicationController
-    
-    # ユーザアカウント新規作成
-    def create
-      user = User.new(user_params)
-      
-      if user.save
-        render json: { status: 'User created successfully' }, status: :created
-      else
-        # より具体的なエラーメッセージを返すように変更
-        render json: { errors: user.errors.full_messages, code: 400 }, status: :bad_request
-      end
-    end
+  before_action :set_cors_headers
 
-    # メールアドレスからユーザIDを取得
-    def find_user_id
-      user = User.find_by(email: params[:email])
-      
-      if user
-        render json: { id: user.id, name: user.name }, status: :ok
-      else
-        render json: { error: 'メールアドレスが存在しません', code: 'auth/user-not-found' }, status: :not_found
-      end
-    end
+  # ユーザアカウント新規作成
+  def create
+    user = User.new(user_params)
     
-      private
-    
-    # 入力フォームで取得したパラメータ受け取り
-    def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    if user.save
+      render json: { status: 'User created successfully' }, status: :created
+    else
+      render json: { errors: user.errors.full_messages, code: 400 }, status: :bad_request
     end
+  end
+
+  # メールアドレスからユーザIDを取得
+  def find_user_id
+    user = User.find_by(email: params[:email])
+    
+    if user
+      render json: { id: user.id, name: user.name }, status: :ok
+    else
+      render json: { error: 'メールアドレスが存在しません', code: 'auth/user-not-found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def set_cors_headers
+    response.set_header('Access-Control-Allow-Origin', '*')
+  end
+
+  # 入力フォームで取得したパラメータ受け取り
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
 end
