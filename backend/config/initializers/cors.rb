9a219ff5364912a8fe3c533a4b809b0cdc973319
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-06-03 21:48:24 +0900

    ‘cors.rbを記述’

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8000"
+    origins 'https://one_drink.vercel.app'
     # origins "*"
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true
   end
 end
