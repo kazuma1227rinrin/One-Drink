80c25a447ab978157ac9ed0f33f40c409006c71e
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-06-04 12:32:35 +0900

    cors.rb作成によりapplication.rbでのCORS設定削除

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -6,6 +6,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Access-Control-Allow-Origin'],
+      max_age: 600,
       credentials: true
   end
 end
