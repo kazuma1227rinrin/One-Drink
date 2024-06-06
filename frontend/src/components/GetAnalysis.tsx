9c0bcbcda4c5d7a5c2d66d737911e4403fd809ed
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-06-06 17:55:32 +0900

    診断ゾーンをカード化する

--- frontend/src/components/GetAnalysis.tsx
+++ frontend/src/components/GetAnalysis.tsx
@@ -1,5 +1,6 @@
+// GetAnalysis.tsx
 import {Button, useToast} from "@chakra-ui/react";
-import {styled} from "styled-components";
+import styled from "styled-components";
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useAuth } from '@/contexts/AuthProvider';
@@ -12,62 +13,65 @@ interface GetAnalysisProps {
     drinkSize: string;
 }
 
-export const GetAnalysis=({ budget, hasNotCaffeine, feeling, commitment, drinkSize }: GetAnalysisProps)=>{
-
+export const GetAnalysis = ({ budget, hasNotCaffeine, feeling, commitment, drinkSize }: GetAnalysisProps) => {
     const router = useRouter();
     const { userId, userName } = useAuth();
     const toast = useToast();
-    console.log(userId,userName);         
+    console.log(userId, userName);
 
     const handleAnalysis = async () => {
+        // drinkSizeがnullまたはundefinedであれば、"tall"を設定
+        const effectiveDrinkSize = drinkSize || "tall";
 
-        // drinkSizeがnullまたはundefinedであれば、"tall"を設定        
-        const effectiveDrinkSize = drinkSize || "tall";  
-        
         try {
-          const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/drink/${userId}`, { 
-            budget, 
-            hasNotCaffeine, 
-            feeling, 
-            commitment, 
-            drinkSize: effectiveDrinkSize
-          });
-
-          if (response.data.error) {
-            toast({
-              title: 'エラー',
-              description: response.data.error,
-              status: 'error',
-              duration: 9000,
-              isClosable: true,
-              position: 'top'
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/drink/${userId}`, {
+                budget,
+                hasNotCaffeine,
+                feeling,
+                commitment,
+                drinkSize: effectiveDrinkSize
             });
-          } else {
-            router.push('/Result'); 
-          }
+
+            if (response.data.error) {
+                toast({
+                    title: 'エラー',
+                    description: response.data.error,
+                    status: 'error',
+                    duration: 9000,
+                    isClosable: true,
+                    position: 'top'
+                });
+            } else {
+                router.push('/Result');
+            }
         } catch (error) {
-          console.error('Error posting budget to backend', error);
-          toast({
-            title: '商品が見つかりませんでした！',
-            description: '他の条件で試してみてください。',
-            status: 'error',
-            duration: 9000,
-            isClosable: true,
-            // position: 'top'
-          });
+            console.error('Error posting budget to backend', error);
+            toast({
+                title: '商品が見つかりませんでした！',
+                description: '他の条件で試してみてください。',
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+            });
         }
     };
 
-    return(
-        <Sdiv>
+    return (
+        <Card>
             <Button onClick={handleAnalysis} colorScheme='orange'>診断結果を見る！</Button>
-        </Sdiv>
-    )
-}
+        </Card>
+    );
+};
 
-const Sdiv = styled.div`
+const Card = styled.div`
     display: flex;
-    justify-content: center; 
-    margin-top: 60px; 
-    margin-bottom: 20px; 
-`
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 60px;
+    margin-bottom: 20px;
+    padding: 20px;
+    background-color: white;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 -4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+`;
