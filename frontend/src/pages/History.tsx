4d88aa6c1a5ce5284d3581336f42d040dc8bcb61
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-06-06 19:30:31 +0900

    「診断画面に戻る」ボタンとカードの左端を揃える

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -77,16 +77,12 @@ const History = () => {
     };
 
     const toggleFavorite = async (id: string) => {
-        // 現在のお気に入り状態を反転させる
         const drinkToUpdate = drinks.find(drink => drink.id === id);
-        if (!drinkToUpdate) return;  // ドリンクが見つからない場合は何もしない
-    
+        if (!drinkToUpdate) return;
         const newFavStatus = !drinkToUpdate.isFavoriteFlg;
         try {
-            // サーバーに新しい状態を送信
             const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/drinks/favorite/${id}`, { isFavorite: newFavStatus });
             if (response.status === 200) {
-                // UIを更新する
                 setDrinks(drinks.map(drink => drink.id === id ? { ...drink, isFavoriteFlg: newFavStatus } : drink));
             }
         } catch (error) {
@@ -96,111 +92,110 @@ const History = () => {
 
     return (
         <ProtectedPage>
-        <>
-            <Header />
-            <ChakraProvider>
-                <Container>
-                    <StyledFlex>
+            <>
+                <Header />
+                <ChakraProvider>
+                    <Container>
                         <Link href="/Analyze" passHref>
                             <Button as="a">診断画面に戻る</Button>
                         </Link>
-                    </StyledFlex>
-                    <TitleHistory />
-                    <Flex justifyContent="center" alignItems="center" mb={4}>
-                        <Text mr={2}>お気に入りのみ表示:</Text>
-                        <Switch
-                            isChecked={showFavorites}
-                            onChange={(e) => setShowFavorites(e.target.checked)}
-                            colorScheme="teal"
-                        />
-                    </Flex>
-                    <Flex direction="column" alignItems="center" width="100%">
-                        {drinks.filter(drink => !showFavorites || drink.isFavoriteFlg).map(drink => (
-                            <Box 
-                                position="relative" 
-                                key={drink.id} 
-                                p="5" 
-                                m="2" 
-                                boxShadow="0 4px 8px rgba(0, 0, 0, 0.1), 0 -4px 8px rgba(0, 0, 0, 0.1)" // 4辺に影を追加
-                                borderRadius="md" // カードの角を少し丸める
-                                background="white" // 背景色を白に設定
-                                width={{ base: "90%", md: "80%", lg: "60%" }} // 幅を調整して中央に収める
-                            >
-                                <IconButton
-                                    aria-label="Delete drink"
-                                    icon={<CloseIcon />}
-                                    colorScheme="gray"
-                                    position="absolute"
-                                    top="1"
-                                    right="1"
-                                    borderRadius="full"
-                                    size="sm"
-                                    onClick={() => { setDeleteId(drink.id); onOpen(); }}
-                                />
-                                <IconButton
-                                    aria-label="Toggle favorite"
-                                    icon={<StarIcon />}
-                                    colorScheme={drink.isFavoriteFlg ? "yellow" : "gray"}
-                                    onClick={() => toggleFavorite(drink.id)}
-                                />
-                                <HStack align="center" spacing="5">
-                                    <VStack>
-                                        <Image src={`https://product.starbucks.co.jp${drink.image}`} boxSize="150px" alt="商品画像"/>
-                                        <Text fontSize="lg">{drink.name}({drink.size})</Text>
-                                        <Box>
-                                            <Badge colorScheme="green">カロリー: {drink.calories}</Badge>
-                                            <Badge ml="2" colorScheme="blue">タンパク質: {drink.protein}</Badge>
-                                        </Box>
-                                        <Box textAlign="left">
-                                            {drink.customs && drink.customs.map((custom, index) => (
-                                                <Text key={index}>・{custom}</Text>
-                                            ))}
-                                        </Box>
-                                        <Link href={`/CustomFromHistory/${drink.id}`} passHref>
-                                            <Button as="a" colorScheme="blue">カスタム編集</Button>
-                                        </Link>
-                                    </VStack>
-                                    <VStack align="start" width="100%" maxW="600px">
-                                        <Text fontSize="md" fontStyle="italic">商品説明: {removeHtmlTags(drink.description)}</Text>
-                                        {drink.comments && <Text fontSize="md" fontStyle="italic">コメント: {drink.comments}</Text>}
-                                        <HStack spacing="10" align="right">
-                                            <Link href={`/CommentEdit/${drink.id}`} passHref>
-                                                <Button as="a" colorScheme="green">コメント編集</Button>
+                        <TitleHistory />
+                        <Flex justifyContent="center" alignItems="center" mb={4}>
+                            <Text mr={2}>お気に入りのみ表示:</Text>
+                            <Switch
+                                isChecked={showFavorites}
+                                onChange={(e) => setShowFavorites(e.target.checked)}
+                                colorScheme="teal"
+                            />
+                        </Flex>
+                        <Flex direction="column" alignItems="center" width="100%">
+                            {drinks.filter(drink => !showFavorites || drink.isFavoriteFlg).map(drink => (
+                                <Box 
+                                    position="relative" 
+                                    key={drink.id} 
+                                    p="5" 
+                                    m="2" 
+                                    boxShadow="0 4px 8px rgba(0, 0, 0, 0.1), 0 -4px 8px rgba(0, 0, 0, 0.1)" 
+                                    borderRadius="md" 
+                                    background="white" 
+                                    width="100%" 
+                                    maxWidth="800px"
+                                >
+                                    <IconButton
+                                        aria-label="Delete drink"
+                                        icon={<CloseIcon />}
+                                        colorScheme="gray"
+                                        position="absolute"
+                                        top="1"
+                                        right="1"
+                                        borderRadius="full"
+                                        size="sm"
+                                        onClick={() => { setDeleteId(drink.id); onOpen(); }}
+                                    />
+                                    <IconButton
+                                        aria-label="Toggle favorite"
+                                        icon={<StarIcon />}
+                                        colorScheme={drink.isFavoriteFlg ? "yellow" : "gray"}
+                                        onClick={() => toggleFavorite(drink.id)}
+                                    />
+                                    <HStack align="center" spacing="5">
+                                        <VStack>
+                                            <Image src={`https://product.starbucks.co.jp${drink.image}`} boxSize="150px" alt="商品画像"/>
+                                            <Text fontSize="lg">{drink.name}({drink.size})</Text>
+                                            <Box>
+                                                <Badge colorScheme="green">カロリー: {drink.calories}</Badge>
+                                                <Badge ml="2" colorScheme="blue">タンパク質: {drink.protein}</Badge>
+                                            </Box>
+                                            <Box textAlign="left">
+                                                {drink.customs && drink.customs.map((custom, index) => (
+                                                    <Text key={index}>・{custom}</Text>
+                                                ))}
+                                            </Box>
+                                            <Link href={`/CustomFromHistory/${drink.id}`} passHref>
+                                                <Button as="a" colorScheme="blue">カスタム編集</Button>
                                             </Link>
-                                        </HStack>
-                                    </VStack>
-                                </HStack>
-                            </Box>
-                        ))}
-                    </Flex>
-                </Container>
-                <AlertDialog
-                    isOpen={isOpen}
-                    leastDestructiveRef={cancelRef}
-                    onClose={onClose}
-                >
-                    <AlertDialogOverlay>
-                        <AlertDialogContent>
-                            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                                ドリンク削除確認
-                            </AlertDialogHeader>
-                            <AlertDialogBody>
-                                このドリンクを削除してもよろしいですか？
-                            </AlertDialogBody>
-                            <AlertDialogFooter>
-                                <Button ref={cancelRef} onClick={onClose}>
-                                    キャンセル
-                                </Button>
-                                <Button colorScheme="red" onClick={handleDelete} ml={3}>
-                                    削除
-                                </Button>
-                            </AlertDialogFooter>
-                        </AlertDialogContent>
-                    </AlertDialogOverlay>
-                </AlertDialog>
-            </ChakraProvider>
-            <Footer />
-        </>
+                                        </VStack>
+                                        <VStack align="start" width="100%" maxW="600px">
+                                            <Text fontSize="md" fontStyle="italic">商品説明: {removeHtmlTags(drink.description)}</Text>
+                                            {drink.comments && <Text fontSize="md" fontStyle="italic">コメント: {drink.comments}</Text>}
+                                            <HStack spacing="10" align="right">
+                                                <Link href={`/CommentEdit/${drink.id}`} passHref>
+                                                    <Button as="a" colorScheme="green">コメント編集</Button>
+                                                </Link>
+                                            </HStack>
+                                        </VStack>
+                                    </HStack>
+                                </Box>
+                            ))}
+                        </Flex>
+                    </Container>
+                    <AlertDialog
+                        isOpen={isOpen}
+                        leastDestructiveRef={cancelRef}
+                        onClose={onClose}
+                    >
+                        <AlertDialogOverlay>
+                            <AlertDialogContent>
+                                <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                                    ドリンク削除確認
+                                </AlertDialogHeader>
+                                <AlertDialogBody>
+                                    このドリンクを削除してもよろしいですか？
+                                </AlertDialogBody>
+                                <AlertDialogFooter>
+                                    <Button ref={cancelRef} onClick={onClose}>
+                                        キャンセル
+                                    </Button>
+                                    <Button colorScheme="red" onClick={handleDelete} ml={3}>
+                                        削除
+                                    </Button>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialogOverlay>
+                    </AlertDialog>
+                </ChakraProvider>
+                <Footer />
+            </>
         </ProtectedPage>
     );
 };
@@ -210,12 +205,7 @@ const Container = styled.div`
     max-width: 800px;
     margin-left: auto;
     margin-right: auto;
-`;
-
-const StyledFlex = styled(Flex)`
-    justify-content: flex-start;
-    width: 100%;
-    margin-top: 8px;
+    padding: 0 20px;
 `;
 
 export default History;
