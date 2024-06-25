8adadcc83e6c36b9e0a5b110f0a7a7cbfde22e1f
榊原一馬 <kzm@sakakibarakazumanoMacBook-Pro.local>
2024-06-25 20:34:57 +0900

    メールアドレスにドットが2つ含まれる形式でも登録できるように修正

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -135,7 +135,7 @@ export default function SignUpScreen() {
                         message: '50文字以内で入力してください',
                       },
                       pattern: {
-                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@+[a-zA-Z0-9-]+\.+[a-zA-Z0-9-]+$/,
+                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/,
                         message: 'メールアドレスの形式が違います',
                       },
                     })}
